ac9ddc6791e1b4bb6d3c015b72a9dabd60a418bc
junaid-home <jj123dev@gmail.com>
2021-12-17 12:24:04 +0500

    feat: scaffold react-native project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
